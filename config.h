2ad280b72826321e3f41aa654b3b023c9be614c4
luck07051 <luck0751@gmail.com>
2022-12-14 22:17:40 +0800

    patch externalpipe.

--- config.h
+++ config.h
@@ -231,6 +231,7 @@ static MouseShortcut mshortcuts[] = {
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
+#define EXPIPE(cmd) { .v = (const char*[]) { "/bin/sh", "-c", cmd, "externalpipe", NULL } }
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -248,6 +249,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_O,           externalpipe,   EXPIPE("st-copyout -p 2") },
+	{ TERMMOD,              XK_U,           externalpipe,   EXPIPE("st-urlhandler -c") },
 };
 
 /*
